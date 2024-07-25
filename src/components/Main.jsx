895a67b2870aedfc0a5068dd9f6326d15e42167b
Jaewoo <qkrwodn4848@naver.com>
2024-07-25 22:04:06 +0900

    Home.jsx 간략화

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Slider from "./Slider";
+import Card from "./Card";
+import Image from "./Image";
+import ImgText from "./ImgText";
+import Banner from "../components/Banner";
+import Text from "../components/Text";
 
-const Main = ({ children }) => {
+const Main = () => {
   return (
     <main id="main" role="main">
-      {children}
+      <Slider element="nexon" />
+      <Image element="section nexon" title="FDW에 오신 걸 환영합니다!" />
+      <Card element="section nexon" title="공연 후기" />
+      <ImgText element="section nexon gray" title="이미지 텍스트 유형" />
+      {/* <Banner element="nexon" title="배너 영역" />
+        <Text element="section nexon" title="텍스트 유형01" /> */}
     </main>
   );
 };
+
+// const Main = ({ children }) => {
+//   return (
+//     <main id="main" role="main">
+//       {children}
+//     </main>
+//   );
+// };
 
 export default Main;
