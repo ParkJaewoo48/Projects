af17f910095953f59ee048c697c108c800e9c6a3
Jaewoo <qkrwodn4848@naver.com>
2024-09-07 09:35:31 +0900

    .

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -50,3 +50,48 @@ const Preview = (props) => {
 };
 
 export default Preview;
+
+{
+  /* <section id="imageType" className={`imageType__wrap ${props.element}`}>
+<div className="image__inner container">
+  <post className="image img1">
+    <h3 className="image__title">인디토크</h3>
+    <p className="image__desc">
+      <PostList />
+    </p>
+    <NavLink className="image__btn" to="/talk">
+      자세히 보기
+    </NavLink>
+  </post>
+  <post className="image img2">
+    <h3 className="image__title">같이봐요</h3>
+    <p className="image__desc">
+      <PostList />
+    </p>
+    <NavLink className="image__btn yellow" to="/together">
+      자세히 보기
+    </NavLink>
+  </post>
+</div>
+<div className="image__inner container">
+  <post className="image img1">
+    <h3 className="image__title">공연후기</h3>
+    <p className="image__desc">
+      <PostList />
+    </p>
+    <NavLink className="image__btn skyblue" to="/review_perform">
+      자세히 보기
+    </NavLink>
+  </post>
+  <post className="image img2">
+    <h3 className="image__title">공연홍보</h3>
+    <p className="image__desc">
+      <PostList />
+    </p>
+    <NavLink className="image__btn grey" to="/promote_perform">
+      자세히 보기
+    </NavLink>
+  </post>
+</div>
+</section> */
+}
