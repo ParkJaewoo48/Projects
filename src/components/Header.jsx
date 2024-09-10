b33c6e7678d7296eed6abf391d75394a6b124fe8
JaewooPark <qkrwodn4848@naver.com>
2024-09-10 12:40:16 +0900

    헤더 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,6 +31,7 @@ const Header = (props) => {
           </ul>
         </nav>
         <div className="header__member">
+          <NavLink to="/mypage">마이페이지</NavLink>
           <NavLink to="/login">로그인</NavLink>
           <NavLink to="/signup">회원가입</NavLink>
         </div>
